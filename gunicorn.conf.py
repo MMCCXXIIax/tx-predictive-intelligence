956f7aac1fa7d974588b39799551a6e7b82a3181
Robert Manegabe <143917676+MMCCXXIIax@users.noreply.github.com>
2025-09-17 14:41:10 +0300

    Update gunicorn.conf.py
    jjjjjj

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,7 +2,8 @@
 import os
 
 # Server socket
-bind = f"0.0.0.0:{os.environ.get('PORT', 5000)}"
+port = int(os.environ.get('PORT', 5000))
+bind = f"0.0.0.0:{port}"
 backlog = 2048
 
 # Worker processes
@@ -20,6 +21,7 @@ max_requests_jitter = 50
 accesslog = '-'
 errorlog = '-'
 loglevel = 'info'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 # Process naming
 proc_name = 'tx-trade-whisperer'
@@ -27,3 +29,9 @@ proc_name = 'tx-trade-whisperer'
 # Server mechanics
 preload_app = True
 daemon = False
+pidfile = None
+tmp_upload_dir = None
+
+# SSL (if needed)
+keyfile = None
+certfile = None
